2c645655ba9b6b82998680a1426291890c25744f
Silvio Junior Carvalho <silviojraraujo@gmail.com>
2022-10-02 22:47:36 -0300

    Ajustes de layout

--- scripts.js
+++ scripts.js
@@ -1,15 +1,19 @@
-async function getHTML() {
+async function start() {
     const cors_anywhere = 'https://cors-anywhere.herokuapp.com/'
     const target = 'https://www.globo.com/'
     const url = cors_anywhere+target
+    const app = document.querySelector('main')
     await fetch(url)
     .then(response => response.text())
     .then(text => text.match(/<(\h2 class="post__title"+)[\s\S]*?>([\s\S]*?)<\/\h2>/g))
     .then(list => list.forEach(item => {
         let p = document.createElement('p')
         p.innerHTML = item
-        document.querySelector('main').append(p)
+        app.append(p)
     }))
+    const hr = document.createElement('hr')
+    app.append(hr)
+
 }
 
-getHTML();
+document.addEventListener("DOMContentLoaded", start);
