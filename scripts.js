eb4f945fb0be4e14397fca12024e6fdc7ec0e62b
Silvio Junior Carvalho <silviojraraujo@gmail.com>
2022-10-02 23:11:43 -0300

    Tratamento de erros

--- scripts.js
+++ scripts.js
@@ -3,14 +3,23 @@ async function start() {
     const target = 'https://www.globo.com/'
     const url = cors_anywhere+target
     const app = document.querySelector('main')
+    const loading = document.createElement('h2')
+    loading.innerText = 'Carregando dados...'
+    app.append(loading)
     await fetch(url)
     .then(response => response.text())
     .then(text => text.match(/<(\h2 class="post__title"+)[\s\S]*?>([\s\S]*?)<\/\h2>/g))
     .then(list => list.forEach(item => {
-        let p = document.createElement('p')
-        p.innerHTML = item
-        app.append(p)
+        let news = document.createElement('div')
+        news.innerHTML = item
+        app.append(news)
     }))
+    .catch(() => {
+        let error = document.createElement('h2')
+        error.innerHTML = 'Para carregar esta página é necessário que você solicite um acesso ao proxy neste <a href="https://cors-anywhere.herokuapp.com/corsdemo" target="_blank">link</a>, depois volte e atualize essa página :)'
+        app.append(error)
+    })
+    .finally(() => loading.remove())
     const hr = document.createElement('hr')
     app.append(hr)
 
